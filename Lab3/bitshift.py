bdac70468b67d30d9a6af5a4ad11ef6f51ddbf9a
Minnie Smith <jacta-alea-est@hotmail.com>
2019-08-24 08:06:18 +0800

    bitshift

--- Lab3/bitshift.py
+++ Lab3/bitshift.py
@@ -1,19 +1,45 @@
 def bitshift(s, k, b):
     string = []
-    newString = []
+
     length = len(s)
     print(length)
 
     for x in s:
         string.append(x)
+    print(string)
 
-    if (b == True):
-        for i in string:
-            newString[i] = string[((i+k) % length)]
-        return newString
-    else:
-        for i in string:
-            newString[((i+k) % length)] = string[i]
-        return newString
+    if b == False:
+        for i in range(k):
+            string[-k] = string[i]
+        print(string)
 
-print (bitshift("100100", 1, True))
+
+bitshift("101100", 1, False)
+
+bitshift("100001", 2, False)
+    # if b == True:
+    #     for i in range(length):
+    #         string[i], string[-k] = string[-k], string[-i]
+    #     print(string)
+
+
+
+
+
+
+
+    # for i in range(length):
+    #     string[i] = string[(i + k) % length]
+    # print(string)
+
+    # if (b == True):
+    #     for i in string:
+    #         newString.append(string[((i+int(k)) % length)])
+    #     return newString
+    # else:
+    #     for i in string:
+    #         newString[((i+int(k)) % length)] = string[i]
+    #     return newString
+
+
+
